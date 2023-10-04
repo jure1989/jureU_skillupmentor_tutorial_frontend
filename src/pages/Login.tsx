b16ae83a1c9800113d159cc9945f794e4284013e
jure1989 <jure.uran@gmail.com>
2023-10-04 11:42:32 +0200

    DasboardLayout.tsx, Sidebar.tsx, Topbar.tsx, Login.tsx, Register.tsx, index.tsx, Routes.tsx

---
+++ src/pages/Login.tsx
@@ -0,0 +1,8 @@
+import Layout from 'components/ui/Layout'
+import { FC } from 'react'
+
+const Login: FC = () => {
+  return <Layout>{/*LoginForm*/}Login</Layout>
+}
+
+export default Login
---
+++ src/pages/Register.tsx
@@ -0,0 +1,8 @@
+import Layout from 'components/ui/Layout'
+import { FC } from 'react'
+
+const Register: FC = () => {
+  return <Layout>{/*RegisterForm*/}Register</Layout>
+}
+
+export default Register
