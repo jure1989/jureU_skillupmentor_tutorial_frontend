747428fb6a89d2b78e181d857a5966c491fcd3e0
jure1989 <jure.uran@gmail.com>
2023-11-29 18:50:47 +0100

    navbar

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,7 +2,13 @@ import DashboardLayout from 'components/ui/DashboardLayout'
 import { FC } from 'react'
 
 const Dashboard: FC = () => {
-  return <DashboardLayout>index</DashboardLayout>
+  return (
+    <DashboardLayout>
+      <h1 className="mb-4">Sales</h1>
+
+      <div id="chart"></div>
+    </DashboardLayout>
+  )
 }
 
 export default Dashboard
