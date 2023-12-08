78aaecfe1717ee02baab7326aa727d8b0edd1dc1
jure1989 <jure.uran@gmail.com>
2023-12-08 18:47:31 +0100

    LoginForm,RegisterForm

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,13 @@
 import Layout from 'components/ui/Layout'
+import LoginForm from 'components/User/LoginForm'
 import { FC } from 'react'
 
 const Login: FC = () => {
-  return <Layout>{/*LoginForm*/}Login</Layout>
+  return (
+    <Layout>
+      <LoginForm />
+    </Layout>
+  )
 }
 
 export default Login
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,13 @@
 import Layout from 'components/ui/Layout'
+import RegisterForm from 'components/User/RegisterForm'
 import { FC } from 'react'
 
 const Register: FC = () => {
-  return <Layout>{/*RegisterForm*/}Register</Layout>
+  return (
+    <Layout>
+      <RegisterForm />
+    </Layout>
+  )
 }
 
 export default Register
