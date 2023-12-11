fc81f4f4507a1d0d8450e6448f9fd6771bd799dd
jure1989 <jure.uran@gmail.com>
2023-12-11 13:50:27 +0100

    Sidebar, Topbar, LoginForm, RegisterForm, useLogin, useRegister

--- src/components/User/LoginForm.tsx
+++ src/components/User/LoginForm.tsx
@@ -14,7 +14,7 @@ import authStore from 'stores/auth.store'
 
 const LoginForm: FC = () => {
   const navigate = useNavigate()
-  const { handleSubmit, errors, reset, control } = useLoginForm()
+  const { handleSubmit, errors, control } = useLoginForm()
   const [apiError, setApiError] = useState('')
   const [showError, setShowError] = useState(false)
 
