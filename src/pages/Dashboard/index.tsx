c7523e724e2db75926de03c78a6c23165e2cf1b3
jure1989 <jure.uran@gmail.com>
2024-01-10 15:40:19 +0100

    index.tsx

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,40 @@
 import DashboardLayout from 'components/ui/DashboardLayout'
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
+import * as C3 from 'c3'
+import * as API from 'api/Api'
 
 const Dashboard: FC = () => {
+  useEffect(() => {
+    ;(async () => {
+      const chart = C3.generate({
+        bindto: '#chart',
+        data: {
+          x: 'x',
+          columns: [['x'], ['Sales']],
+          types: {
+            Sales: 'bar',
+          },
+        },
+        axis: {
+          x: {
+            type: 'timeseries',
+            tick: {
+              format: '%Y-%m-%d',
+            },
+          },
+        },
+      })
+
+      const { data } = await API.fetchChart()
+      chart.load({
+        columns: [
+          ['x', ...data.map((r: { date: string; sum: string }) => r.date)],
+          ['Sales', ...data.map((r: { date: string; sum: string }) => r.sum)],
+        ],
+      })
+    })()
+  }, [])
+
   return (
     <DashboardLayout>
       <h1 className="mb-4">Sales</h1>
